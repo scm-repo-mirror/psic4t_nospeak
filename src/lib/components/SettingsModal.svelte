296c9d97d955d25b05bf4d93e433d1f97d17f79d
psic4t <psic4t@data.haus>
2025-12-15 17:09:55 +0100

    Use plus icon for messaging relay add button

--- src/lib/components/SettingsModal.svelte
+++ src/lib/components/SettingsModal.svelte
@@ -910,7 +910,7 @@
                   class="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition-colors disabled:opacity-50 disabled:cursor-not-allowed"
                   disabled={isSavingRelays}
                 >
-                  {$t('settings.messagingRelays.addButton')}
+                  +
                 </button>
               </div>
 
