ad48ab211c67a4093dcdf90db1fdab28606604b7
psic4t <psic4t@data.haus>
2026-01-07 15:12:46 +0100

    license

--- src/lib/components/SettingsModal.svelte
+++ src/lib/components/SettingsModal.svelte
@@ -1683,10 +1683,20 @@
                   >
                     License
                   </h4>
-                  <p class="typ-body text-gray-600 dark:text-slate-400">GPL</p>
-                  <p class="text-sm text-gray-600 dark:text-slate-400 mt-1">
-                    CC by-nc-nd psic4t
-                  </p>
+                  <p class="typ-body text-gray-600 dark:text-slate-400">
+                  Copyright (C) 2026 psic4t</p>
+                  <br>
+
+                  <p class="typ-body text-gray-600 dark:text-slate-400">
+                  This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.</p>
+                  <br>
+                  <p class="typ-body text-gray-600 dark:text-slate-400">
+                  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.</p>
+
+                  <br>
+                  <p class="typ-body text-gray-600 dark:text-slate-400">
+                  You should have received a copy of the GNU General Public License along with this program. If not, see https://www.gnu.org/licenses/.</p>
+
                 </div>
               </div>
             </div>
